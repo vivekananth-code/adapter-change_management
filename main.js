cd08557195f8a22f14151a7cbcf80344d11747d5
vivekananth-code <vivekananth32@gmail.com>
2021-08-29 04:48:00 +0000

    Lab 5

--- main.js
+++ main.js
@@ -1,61 +1,59 @@
 
   
+
 // Import built-in Node.js package path.
 const path = require('path');
-
 /**
- * Import the ServiceNowConnector class from local Node.js module connector.js
- *   and assign it to constant ServiceNowConnector.
- * When importing local modules, IAP requires an absolute file reference.
- * Built-in module path's join method constructs the absolute filename.
- */
+* Import the ServiceNowConnector class from local Node.js module connector.js
+*   and assign it to constant ServiceNowConnector.
+* When importing local modules, IAP requires an absolute file reference.
+* Built-in module path's join method constructs the absolute filename.
+*/
 const ServiceNowConnector = require(path.join(__dirname, '/connector.js'));
-
 /**
- * Import built-in Node.js package events' EventEmitter class and
- * assign it to constant EventEmitter. We will create a child class
- * from this class.
- */
+* Import built-in Node.js package events' EventEmitter class and
+* assign it to constant EventEmitter. We will create a child class
+* from this class.
+*/
 const EventEmitter = require('events').EventEmitter;
-
 /**
- * The ServiceNowAdapter class.
- *
- * @summary ServiceNow Change Request Adapter
- * @description This class contains IAP adapter properties and methods that IAP
- *   brokers and products can execute. This class inherits the EventEmitter
- *   class.
- */
+* The ServiceNowAdapter class.
+*
+* @summary ServiceNow Change Request Adapter
+* @description This class contains IAP adapter properties and methods that IAP
+*   brokers and products can execute. This class inherits the EventEmitter
+*   class.
+*/
 class ServiceNowAdapter extends EventEmitter {
 
-  /**
-   * Here we document the ServiceNowAdapter class' callback. It must follow IAP's
-   *   data-first convention.
-   * @callback ServiceNowAdapter~requestCallback
-   * @param {(object|string)} responseData - The entire REST API response.
-   * @param {error} [errorMessage] - An error thrown by REST API call.
-   */
+/**
+* Here we document the ServiceNowAdapter class' callback. It must follow IAP's
+*   data-first convention.
+* @callback ServiceNowAdapter~requestCallback
+* @param {(object|string)} responseData - The entire REST API response.
+* @param {error} [errorMessage] - An error thrown by REST API call.
+*/
 
-  /**
-   * Here we document the adapter properties.
-   * @typedef {object} ServiceNowAdapter~adapterProperties - Adapter
-   *   instance's properties object.
-   * @property {string} url - ServiceNow instance URL.
-   * @property {object} auth - ServiceNow instance credentials.
-   * @property {string} auth.username - Login username.
-   * @property {string} auth.password - Login password.
-   * @property {string} serviceNowTable - The change request table name.
-   */
+/**
+* Here we document the adapter properties.
+* @typedef {object} ServiceNowAdapter~adapterProperties - Adapter
+*   instance's properties object.
+* @property {string} url - ServiceNow instance URL.
+* @property {object} auth - ServiceNow instance credentials.
+* @property {string} auth.username - Login username.
+* @property {string} auth.password - Login password.
+* @property {string} serviceNowTable - The change request table name.
+*/
 
-  /**
-   * @memberof ServiceNowAdapter
-   * @constructs
-   *
-   * @description Instantiates a new instance of the Itential ServiceNow Adapter.
-   * @param {string} id - Adapter instance's ID.
-   * @param {ServiceNowAdapter~adapterProperties} adapterProperties - Adapter instance's properties object.
-   */
-  constructor(id, adapterProperties) {
+/**
+* @memberof ServiceNowAdapter
+* @constructs
+*
+* @description Instantiates a new instance of the Itential ServiceNow Adapter.
+* @param {string} id - Adapter instance's ID.
+* @param {ServiceNowAdapter~adapterProperties} adapterProperties - Adapter instance's properties object.
+*/
+constructor(id, adapterProperties) {
     // Call super or parent class' constructor.
     super();
     // Copy arguments' values to object properties.
@@ -68,102 +66,93 @@ class ServiceNowAdapter extends EventEmitter {
       password: this.props.auth.password,
       serviceNowTable: this.props.serviceNowTable
     });
-  }
+}
 
-  /**
-   * @memberof ServiceNowAdapter
-   * @method connect
-   * @summary Connect to ServiceNow
-   * @description Complete a single healthcheck and emit ONLINE or OFFLINE.
-   *   IAP calls this method after instantiating an object from the class.
-   *   There is no need for parameters because all connection details
-   *   were passed to the object's constructor and assigned to object property this.props.
-   */
-  connect() {
+/**
+* @memberof ServiceNowAdapter
+* @method connect
+* @summary Connect to ServiceNow
+* @description Complete a single healthcheck and emit ONLINE or OFFLINE.
+*   IAP calls this method after instantiating an object from the class.
+*   There is no need for parameters because all connection details
+*   were passed to the object's constructor and assigned to object property this.props.
+*/
+connect() {
     // As a best practice, Itential recommends isolating the health check action
     // in its own method.
     this.healthcheck();
-  }
+}
 
-  /**
-   * @memberof ServiceNowAdapter
-   * @method healthcheck
-   * @summary Check ServiceNow Health
-   * @description Verifies external system is available and healthy.
-   *   Calls method emitOnline if external system is available.
-   *
-   * @param {ServiceNowAdapter~requestCallback} [callback] - The optional callback
-   *   that handles the response.
-   */
-  healthcheck(callback) {
+/**
+* @memberof ServiceNowAdapter
+* @method healthcheck
+* @summary Check ServiceNow Health
+* @description Verifies external system is available and healthy.
+*   Calls method emitOnline if external system is available.
+*
+* @param {ServiceNowAdapter~requestCallback} [callback] - The optional callback
+*   that handles the response.
+*/
+healthcheck(callback) {
     // We will build this method in a later lab. For now, it will emulate
     // a healthy integration by emmitting ONLINE.
     this.emitOnline();
-  }
+}
 
-  /**
-   * @memberof ServiceNowAdapter
-   * @method emitOffline
-   * @summary Emit OFFLINE
-   * @description Emits an OFFLINE event to IAP indicating the external
-   *   system is not available.
-   */
-  emitOffline() {
+/**
+* @memberof ServiceNowAdapter
+* @method emitOffline
+* @summary Emit OFFLINE
+* @description Emits an OFFLINE event to IAP indicating the external
+*   system is not available.
+*/
+emitOffline() {
     this.emitStatus('OFFLINE');
     log.warn('ServiceNow: Instance is unavailable.');
-  }
+}
 
-  /**
-   * @memberof ServiceNowAdapter
-   * @method emitOnline
-   * @summary Emit ONLINE
-   * @description Emits an ONLINE event to IAP indicating external
-   *   system is available.
-   */
-  emitOnline() {
+/**
+* @memberof ServiceNowAdapter
+* @method emitOnline
+* @summary Emit ONLINE
+* @description Emits an ONLINE event to IAP indicating external
+*   system is available.
+*/
+emitOnline() {
     this.emitStatus('ONLINE');
     log.info('ServiceNow: Instance is available.');
-  }
+}
 
-  /**
-   * @memberof ServiceNowAdapter
-   * @method emitStatus
-   * @summary Emit an Event
-   * @description Calls inherited emit method. IAP requires the event
-   *   and an object identifying the adapter instance.
-   *
-   * @param {string} status - The event to emit.
-   */
-  emitStatus(status) {
+/**
+* @memberof ServiceNowAdapter
+* @method emitStatus
+* @summary Emit an Event
+* @description Calls inherited emit method. IAP requires the event
+*   and an object identifying the adapter instance.
+*
+* @param {string} status - The event to emit.
+*/
+emitStatus(status) {
     this.emit(status, { id: this.id });
-  }
+}
 
-  /**
-   * @memberof ServiceNowAdapter
-   * @method getRecord
-   * @summary Get ServiceNow Record
-   * @description Retrieves a record from ServiceNow.
-   *
-   * @param {ServiceNowAdapter~requestCallback} callback - The callback that
-   *   handles the response.
-   */
-  getRecord(callback) {
+/**
+* @memberof ServiceNowAdapter
+* @method getRecord
+* @summary Get ServiceNow Record
+* @description Retrieves a record from ServiceNow.
+*
+* @param {ServiceNowAdapter~requestCallback} callback - The callback that
+*   handles the response.
+*/
+getRecord(callback) {
     /**
      * Write the body for this function.
      * The function is a wrapper for this.connector's get() method.
      * Note how the object was instantiated in the constructor().
      * get() takes a callback function.
      */
-    this.connector.get((data, error) => {
-    if (error) {
-      console.error(`\nError returned from GET request:\n${JSON.stringify(error)}`);
-    }
-
-    console.log(`\nResponse returned from GET request:\n${JSON.stringify(data)}`)
-    var jsonstring = JSON.parse(JSON.stringify(data));
-    return this.setResultsData(jsonstring);
-  })
-  
+    ServiceNowConnector.get(callback);
   }
 
   /**
@@ -182,34 +171,7 @@ class ServiceNowAdapter extends EventEmitter {
      * Note how the object was instantiated in the constructor().
      * post() takes a callback function.
      */
-     return this.connector.post( (data, error) => {
-    if (error) {
-      console.error(`\nError returned from POST request:\n${JSON.stringify(error)}`);
-    }
-    console.log(`\nResponse returned from POST request:\n${JSON.stringify(data)}`)
-     var jsonstring = JSON.parse(JSON.stringify(data));
-    var body=jsonstring.body;
-    var results=JSON.parse(body);
-    return results;
-    
-  });
-  }
-  setResultsData(jsonstring){
-       
-    var body=jsonstring.body;
-    var results=JSON.parse(body);
-    var result={
-        "change_ticket_number":results['result'][0].number,
-            "active": results['result'][0].active,
-            "priority": results['result'][0].priority,
-        "description": results['result'][0].description,
-        "work_start": results['result'][0].work_start,
-        "work_end": results['result'][0].work_end,
-        "change_ticket_key": results['result'][0].sys_id
-
-    }
-    return result;
-    //console.log(result);
+    ServiceNowConnector.post(callback);
   }
 }
 
